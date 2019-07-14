cd5adbc3d00cb71aa7dc5894fa8658989d4d12d3
Alan Nascimento <alan.silva.n@hotmail.com>
2019-07-14 14:08:31 -0300

    adding crud

--- src/controllers/TruckerController.ts
+++ src/controllers/TruckerController.ts
@@ -9,10 +9,28 @@ class TruckerController {
         return res.json(truckers)
     }
 
+    public async show (req: Request, res: Response): Promise<Response> {
+        const trucker = await Trucker.findById(req.params.id)
+
+        return res.json(trucker)
+    }
+
     public async create (req: Request, res: Response): Promise<Response> {
         const trucker = await Trucker.create(req.body)
 
         return res.json(trucker)
+    }
+
+    public async update (req: Request, res: Response): Promise<Response> {
+        const trucker = await Trucker.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        return res.json(trucker)
+    }
+
+    public async delete (req: Request, res: Response): Promise<Response> {
+        await Trucker.findByIdAndRemove(req.params.id)
+
+        return res.send()
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,9 @@ import TruckerController from './controllers/TruckerController'
 const routes = Router()
 
 routes.get('/truckers', TruckerController.index)
+routes.get('/truckers/:id', TruckerController.show)
 routes.post('/truckers', TruckerController.create)
+routes.put('/truckers/:id', TruckerController.update)
+routes.delete('/truckers/:id', TruckerController.delete)
 
 export default routes
